e420b9d4643f0e14a0f9d43aa1005f1821f737c6
sctwomey <sctwomey@gmail.com>
2023-11-15 08:14:36 -0800

    Refactor resolvers file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,35 +1,35 @@
-const { User, Product, Category, Order } = require('../models');
+const { User, Product, Genre, Order } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
-    categories: async () => {
-      return await Category.find();
+    genres: async () => {
+      return await Genre.find();
     },
-    products: async (parent, { category, name }) => {
+    products: async (parent, { genre, title }) => {
       const params = {};
 
-      if (category) {
-        params.category = category;
+      if (genre) {
+        params.genre = genre;
       }
 
-      if (name) {
-        params.name = {
-          $regex: name,
+      if (title) {
+        params.title = {
+          $regex: title,
         };
       }
 
-      return await Product.find(params).populate('category');
+      return await Product.find(params).populate('genre');
     },
     product: async (parent, { _id }) => {
-      return await Product.findById(_id).populate('category');
+      return await Product.findById(_id).populate('genre');
     },
     user: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
           path: 'orders.products',
-          populate: 'category',
+          populate: 'genre',
         });
 
         user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
@@ -43,7 +43,7 @@ const resolvers = {
       if (context.user) {
         const user = await User.findById(context.user._id).populate({
           path: 'orders.products',
-          populate: 'category',
+          populate: 'genre',
         });
 
         return user.orders.id(_id);
@@ -62,7 +62,7 @@ const resolvers = {
           price_data: {
             currency: 'usd',
             product_data: {
-              name: product.name,
+              title: product.title,
               description: product.description,
               images: [`${url}/images/${product.image}`],
             },
