3e682bb6014f0b6c51675974905d895cea262848
sctwomey <sctwomey@gmail.com>
2023-11-21 10:45:04 -0800

    Modify login and signup page styling

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -30,46 +30,48 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
+    <div className="container">
       <Link to="/login">← Go to Login</Link>
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="userName">UserName:</label>
-          <input
-            placeholder="User"
-            name="userName"
-            type="userName"
-            id="userName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
+      <h2 className='signup-login-h2'>Signup</h2>
+      <div className='d-flex justify-content-center'>
+        <form onSubmit={handleFormSubmit} className='signup-login-form'>
+          <div className="form-group">
+            <label htmlFor="userName">UserName:</label>
+            <input
+              placeholder="Your Username"
+              name="userName"
+              type="userName"
+              id="userName"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              placeholder="youremail@email.com"
+              name="email"
+              type="email"
+              id="email"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              placeholder="******"
+              name="password"
+              type="password"
+              id="pwd"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex-row justify-content-center">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+    </div >
   );
 }
 
